0e67c1eed7f79d454327c47dc3874fd7585b3a45
gustavo <105922570+castro047@users.noreply.github.com>
2023-01-12 21:42:17 -0300

    Add files via upload

---
+++ site/js/main.js
@@ -0,0 +1,15 @@
+$('.slider.principal').slick({
+    dots: true,
+    infinite: true, 
+    speed: 300,  
+    slidesToShow:1,
+});
+
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
